806918680cbc6578bb857ab33ab6082f477bb1e6
L.Fork <986204478@qq.com>
2018-08-08 23:46:33 +0800

    init binary search file

---
+++ src/search/binary/BinarySearch.java
@@ -0,0 +1,13 @@
+package search.binary;
+
+/**
+ * @author 98620
+ */
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] array = {-1, 0, 1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9};
+
+
+
+    }
+}
